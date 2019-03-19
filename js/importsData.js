6b61ec17f89a3a783d5a05681dc2617a0b92b970
Jessica Koynock <jk804614@ohio.edu>
2019-03-19 19:23:29 -0400

    Uploading Source Code

---
+++ js/importsData.js
@@ -0,0 +1,131 @@
+var importsData = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "country": "Columbia",
+        "imports": 1254
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -74.29733,
+          4.57086
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "country": "Brazil",
+        "imports": 1073
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -51.92527,
+          -14.235
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "country": "Vietman",
+        "imports": 516
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          108.27719,
+          14.05832
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "country": "Guatemala",
+        "imports": 330
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -90.50688,
+          14.63491
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "country": "Indonesia",
+        "imports": 317
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          113.92132,
+          -0.78927
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "country": "Honduras",
+        "imports": 295
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -86.2419,
+          15.19999
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "country": "Nicaraqua",
+        "imports": 289
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+           -85.20722,
+          12.86541
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "country": "Peru",
+        "imports": 230
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -75.01515,
+          -9.18996
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "country": "Mexico",
+        "imports": 183
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -102.55278,
+          23.6345
+        ]
+      }
+    }
+  ]
+};
